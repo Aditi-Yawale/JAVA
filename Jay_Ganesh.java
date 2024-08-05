a65ceb18b6055f9ea033d6845827c53fdfe72691
Aditi Yawale <yawaleaditi@gmail.com>
2024-08-06 00:21:05 +0530

    Add files via upload

---
+++ Jay_Ganesh.java
@@ -0,0 +1,10 @@
+//Basic code as a headstart to my GitHUB Profile, that prints "Jay Ganesh" on the screen
+
+class Jay_Ganesh
+{
+    public static void main(String Arg[])
+    {
+        System.out.println("Jay Ganesh...");
+    }
+}
+
